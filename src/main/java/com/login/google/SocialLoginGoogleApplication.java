45b9866c46ee2467140673e6893e169676a4664a
Soyeon <isylsy166@gmail.com>
2025-07-17 15:41:15 +0900

    dotenv로 환경변수 읽어오기

--- src/main/java/com/login/google/SocialLoginGoogleApplication.java
+++ src/main/java/com/login/google/SocialLoginGoogleApplication.java
@@ -1,13 +1,20 @@
 package com.login.google;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SocialLoginGoogleApplication {
-
     public static void main(String[] args) {
+
+        // .env 파일 읽기
+        Dotenv dotenv = Dotenv.load();
+
+        // 환경변수로 등록
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
         SpringApplication.run(SocialLoginGoogleApplication.class, args);
     }
-
 }
