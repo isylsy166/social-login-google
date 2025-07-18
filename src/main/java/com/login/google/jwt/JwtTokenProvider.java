2beee3039ec55203efd47684b584e3e742561971
Soyeon <isylsy166@gmail.com>
2025-07-18 09:36:04 +0900

    JWT token provider

---
+++ src/main/java/com/login/google/jwt/JwtTokenProvider.java
@@ -0,0 +1,62 @@
+package com.login.google.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Slf4j
+@Component
+
+public class JwtTokenProvider {
+
+    @Value("${JWT_SECRET}")
+    private String secretKey;
+    private  Key key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String createToken(String email) {
+
+        long EXPIRATION_TIME = 1000 * 60 * 20; // 20분
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getSubject(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            log.warn("JWT 검증 실패: {}", e.getMessage());
+            return false;
+        }
+    }
+
+}
